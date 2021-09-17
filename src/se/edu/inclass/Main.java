247d1e5789a3e348d418e0f84f150bd858f44f39
lezong <lezongmun@gmail.com>
2021-09-17 17:17:44 +0800

    add code from lecture

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,8 +20,12 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        printData(tasksData);
+//        printDataWithStreams(tasksData);
+//        printDeadlinesUsingStreams(tasksData);
 
     }
+
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
         int count = 0;
@@ -33,15 +37,40 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStreams(ArrayList<Task> tasks) {
+
+        int count = (int) tasks.stream()
+                .filter((t) -> t instanceof Deadline)// passing each element to function ...terminal operator
+                .count(); //aggregate function
+
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data by looping");
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasks){
+        System.out.println("Printing deadlines using stream");
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline) // filtering using lambda
+                .forEach(System.out::println);
+    }
+
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using streams");
+        tasks.stream() // convert data to stream
+                .forEach(System.out::println); // passing each element to function ...terminal operator
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+//        ArrayList<Task> deadlines = new ArrayList<>();
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
+//                deadlines.add(t);
                 System.out.println(t);
             }
         }
